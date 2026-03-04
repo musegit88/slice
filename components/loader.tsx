2a1f43b30351ad63c44ffbef9aa5a7af8ec7097e
Muse <musemy8484@gmail.com>
2026-03-04 16:45:54 +0300

    feat: add message in loader component

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-const Loader = () => {
+const Loader = ({ message }: { message?: string }) => {
   return (
-    <div className="fixed inset-0  bg-[#ffffff]/40 backdrop-blur-xs z-50">
-      <div className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-full loader"></div>
+    <div className="relative">
+      <div className="fixed inset-0  bg-[#ffffff]/40 backdrop-blur-xs z-50">
+        <div className="fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-full loader" />
+        <div className="flex items-center justify-center h-full mt-5">
+          {message && (
+            <p className="text-center text-xs sm:text-md md:text-lg">
+              {message}
+            </p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
